59f68051d09c0713f15b75215daec470d6442649
Carl Chapman <carlallenchapman@gmail.com>
2015-12-10 03:36:22 -0600

    creating CSV in progress - rest should be clear but I'm out of steam

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang3.tuple.MutablePair;
 
 public class Main {
 	private static final String filePath = "/Users/carlchapman/Desktop/inputData.txt";
@@ -53,5 +58,53 @@ public class Main {
 		for(Metagroup mg : mgroups){
 			System.out.println(mg);	
 		}
+
+	}
+	
+	
+	private static void testAssignments(){
+		int[] assignments =getAssignments(180, 6, new Random()); 
+		for(int i=0;i<6;i++){
+			int counter = 0;
+			for(int x : assignments){
+				if(x==i){
+					counter++;
+				}
+			}
+			System.out.println(counter);
+		}
+		System.out.println(Arrays.toString(assignments));
+	}
+	
+	public static int[] getAssignments(int rows, int cols, Random gen){
+
+		int assignmentsPerCol = rows/cols;
+		int[] assignments = new int[rows];
+		
+		//ensuring that all cols get only so many assignments
+		List<MutablePair<Integer,Integer>> colCounters = new LinkedList<MutablePair<Integer,Integer>>();
+		for(int i=0;i<cols;i++){
+			colCounters.add( new MutablePair<Integer,Integer>(i,assignmentsPerCol));
+		}
+		// create a set of numbers to assign to some column
+		ArrayList<Integer> availableNumbers = new ArrayList<Integer>(rows);
+		for(int i=0; i< rows; i++){
+			availableNumbers.add(i);
+		}
+		
+		while(!availableNumbers.isEmpty()){
+			int numberToAssign = availableNumbers.remove(gen.nextInt(availableNumbers.size()));	
+			int pairIndex = gen.nextInt(colCounters.size());
+			MutablePair<Integer,Integer> pair = colCounters.get(pairIndex);
+			
+			assignments[numberToAssign] = pair.left;
+			int remaining = pair.right -1;
+			if(remaining == 0){
+				colCounters.remove(pairIndex);
+			}else{
+				pair.right = remaining;
+			}
+		}
+		return assignments;
 	}
 }
--- src/main/Metagroup.java
+++ src/main/Metagroup.java
@@ -3,6 +3,8 @@ package main;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 public class Metagroup {
 	List<SemanticGroup> groups;
 	
@@ -13,10 +15,28 @@ public class Metagroup {
 	public void addGroup(SemanticGroup group){
 		groups.add(group);
 	}
+	
+	public ImmutablePair<String, List<QuestionString>> getPairAt(int index){
+		int rIndex = 0;
+		for(SemanticGroup sg : groups){
+			for(String regex : sg.regexes){
+				if(rIndex++ == index){
+					return new ImmutablePair<String, List<QuestionString>>(regex,sg.strings);
+				}
+			}
+		}
+		return null;
+	}
 
 	@Override
 	public String toString() {
-		return "Metagroup [groups=" + groups + "]";
+		int nRegexes = 0;
+		int nStrings = 0;
+		for(SemanticGroup sg : groups){
+			nRegexes += sg.regexes.size();
+			nStrings += sg.strings.size();
+		}
+		return "Metagroup [ nGroups: " + groups.size() + " nRegexes: " + nRegexes + " nStrings: " + nStrings + " groups=" + groups + "]";
 	}
 	
 	
